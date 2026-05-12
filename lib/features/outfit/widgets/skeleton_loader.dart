cfbf31e821551e5f59fdb0ec13c4b5430681bd2b
Seda Nur Yavuz <sedanuryavz8@gmail.com>
2026-05-12 14:07:58 +0300

    Implemented outfit management feature with OutfitController, OutfitModel, and UI components for outfit selection and display

---
+++ lib/features/outfit/widgets/skeleton_loader.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../utils/outfit_theme.dart';
+
+class SkeletonLoader extends StatefulWidget {
+  final double width;
+  final double height;
+  final BorderRadius? borderRadius;
+
+  const SkeletonLoader({
+    super.key,
+    required this.width,
+    required this.height,
+    this.borderRadius,
+  });
+
+  @override
+  State<SkeletonLoader> createState() => _SkeletonLoaderState();
+}
+
+class _SkeletonLoaderState extends State<SkeletonLoader>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat(reverse: true);
+
+    _colorAnimation = ColorTween(
+      begin: Colors.grey.shade100,
+      end: Colors.grey.shade200,
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _colorAnimation,
+      builder: (context, child) {
+        return Container(
+          width: widget.width,
+          height: widget.height,
+          decoration: BoxDecoration(
+            color: _colorAnimation.value,
+            borderRadius: widget.borderRadius ?? OutfitTheme.borderRadiusM,
+          ),
+        );
+      },
+    );
+  }
+}
